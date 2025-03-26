3f32504013f840d323dc3df70c5a3e684029b4ce
s4veiro <biel14441@gmail.com>
2025-03-26 17:29:49 -0300

    documentação da API implmentada

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const routes = require('./routes/routes.js');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
+
 app.use(express.json());
 
-app.use('/', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api', routes);
 
 module.exports = app;
